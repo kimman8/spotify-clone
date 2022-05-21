58f0affb654ba8bc70e8acdcd37bdbce76bb370e
Kim Yuen <kim.yuen@akqa.com>
2022-05-21 13:24:26 +1000

    sdfsdfsdf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { RecoilRoot } from 'recoil'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-
+// sdfsdf
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
