3661ca26ed522baab3393df878fb3db75d589338
Kim Yuen <kim.yuen@akqa.com>
2022-05-22 13:22:31 +1000

    changed some styling

--- pages/login.js
+++ pages/login.js
@@ -2,13 +2,13 @@ import { getProviders, signIn } from 'next-auth/react'
 import Image from 'next/image'
 function Login({ providers }) {
   return (
-    <div className="flex min-h-screen  w-full flex-col items-center justify-center bg-black">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-5 bg-black">
       <Image
         src="https://links.papareact.com/9xl"
         alt="spotify logo"
         className="mb-5 h-52"
-        width={250}
-        height={250}
+        width={208}
+        height={208}
       />
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
